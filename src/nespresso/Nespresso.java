7afccb6e9c8ea4e7eec39774d2c1f9671c9e0de2
W4nD-0rDz <wandaordonez@gmail.com>
2023-04-03 18:29:45 -0300

    Commit 2

--- src/nespresso/Nespresso.java
+++ src/nespresso/Nespresso.java
@@ -5,6 +5,8 @@
  */
 package nespresso;
 import entidades.Cafetera;
+import java.util.Scanner;
+import servicios.CafeteraServicio;
 
 /**
  *
@@ -16,8 +18,37 @@ public class Nespresso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         Cafetera c1 = new Cafetera(10, 7);
+        CafeteraServicio cs = new CafeteraServicio();
         
+        System.out.println("Ingrese una opcíón");
+        System.out.println("Opción 1: Llenar cafetera.");
+        System.out.println("Opción 2: Servir taza.");
+        System.out.println("Opción 3: Vaciar cafetera.");
+        System.out.println("Opción 4: Agregar café.");
+        int op = leer.nextInt();
+        
+        switch (op) {
+            case 1:
+                cs.llenarCafetera(c1);
+                break;
+            case 2:
+                //cs.servirTaza();
+                break;
+            case 3:
+                cs.vaciarCafetera(c1);
+                break;
+            case 4:
+                cs.agregarCafe(c1);
+                break;
+            case 0:
+                System.out.println("Hasta Pronto");;
+                break;
+            default:
+                System.out.println("Opción incorrecta. Hasta pronto.");
+                break;
+        }
 
         
     
